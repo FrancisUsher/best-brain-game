52096e71db111de3a3b739407512f3f43df90caa
Francis Usher <francis.w.usher@gmail.com>
2017-11-27 02:00:22 -0800

    Add public directory and gave it a boilerplate service worker.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 
 const port = 8080
 
+app.use('/', express.static('public'))
+
 app.get('/', (req, res) => res.send('Welcome to BBG.'))
 
 app.listen(port, () => console.log(`This app is running on port ${port}`))
