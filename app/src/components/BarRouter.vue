c4da6651af9bf4a2c228d6e0ff63213d37c7a38a
Francis Usher <francis.w.usher@gmail.com>
2017-12-27 04:58:52 -0800

    Change Home section to Create section.

--- app/src/components/BarRouter.vue
+++ app/src/components/BarRouter.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   md-bottom-bar.md-accent(md-sync-route)
-    md-bottom-bar-item(to="/" md-label="Home" md-icon="home")
+    md-bottom-bar-item(to="/" md-label="Create" md-icon="question_answer")
     md-bottom-bar-item(to="/explore" md-label="Explore" md-icon="nature_people")
     md-bottom-bar-item(to="/help" md-label="Help" md-icon="live_help")
 </template>
